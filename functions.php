b9350899b1b95a381cbbf7d439559fb8aa03fc6d
Justin Bull <me@justinbull.ca>
2013-06-29 15:42:06 -0400

    move byline, fix img width

--- functions.php
+++ functions.php
@@ -49,7 +49,7 @@ require_once('library/custom-post-type.php'); // you can disable this if you lik
 
 // Thumbnail sizes
 add_image_size( 'bones-thumb-680-soft', 680, 0, false );
-add_image_size( 'bones-thumb-680', 680, 0, true );
+add_image_size( 'bones-thumb-680', 710, 0, true );
 add_image_size( 'bones-thumb-600', 600, 150, true );
 add_image_size( 'bones-thumb-300', 300, 100, true );
 /*
--- index.php
+++ index.php
@@ -13,19 +13,19 @@
 								<header class="article-header">
 
 									<h1 class="h2"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
-									<div><?php
+									<div style="max-height: 300px; overflow: hidden;"><?php
 										if (has_post_thumbnail()) {
 											the_post_thumbnail('bones-thumb-680');
 										}
 									?></div>
-									<p class="byline vcard"><?php
-										printf(__('Posted <time class="updated" datetime="%1$s" pubdate>%2$s</time> by <span class="author">%3$s</span> <span class="amp">&</span> filed under %4$s.', 'bonestheme'), get_the_time('Y-m-j'), get_the_time(get_option('date_format')), bones_get_the_author_posts_link(), get_the_category_list(', '));
-									?></p>
 
 								</header> <!-- end article header -->
 
 								<section class="entry-content clearfix">
 									<?php the_content(); ?>
+									<p class="byline vcard"><?php
+										printf(__('Posted <time class="updated" datetime="%1$s" pubdate>%2$s</time> by <span class="author">%3$s</span> <span class="amp">&</span> filed under %4$s.', 'bonestheme'), get_the_time('Y-m-j'), get_the_time(get_option('date_format')), bones_get_the_author_posts_link(), get_the_category_list(', '));
+									?></p>
 								</section> <!-- end article section -->
 
 								<footer class="article-footer">
